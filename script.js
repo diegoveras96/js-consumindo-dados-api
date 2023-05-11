c6b8c434386e7804a96e17c983bc7968177bed81
Diego Veras <diego.veras96@hotmail.com>
2023-05-11 16:26:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-console.log("Mandando oi pro amigo");
-
-function mandaMensagem(){
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação");
-    console.log("Solicitação Recebida!")
-}
-
-mandaMensagem();
-
-console.log("Tchau! Tchau!")
+var cep = 22743311;
+var consultaCep = fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro){
+            throw Error('esse cep não existe')
+        } else{
+            console.log(r);
+        }
+    })
+    .catch(erro => console.log(erro))
+    .finally( () => console.log('Processamento Concluído'))
